00ad9c39841db989f449d4570bac8dd633959468
nalinjax <nalin_bc@yahoo.com>
2025-09-19 18:13:59 -0400

    Starting on persistance

--- NalinTransactionPersistence/TransactionPersistanceFile.cs
+++ NalinTransactionPersistence/TransactionPersistanceFile.cs
@@ -2,24 +2,73 @@
 // Sep/19/2025 - Jacksonville FL
 
 using NalinTransactionCommon;
+using System.Text.Json;
 
 namespace NalinTransactionPersistence
 {
     /// <summary>
-    /// Persistance via a file on disk
+    /// Persistance to a file on disk
     /// </summary>
     internal class TransactionPersistanceFile : ITransactionPersistance
     {
+        private Object locker = new Object();
+
+        //TODO: Add logging
+
         public bool Persist(TransactionData transactionData)
         {
-            //TODO:
-            throw new NotImplementedException();
+            try
+            {
+                var serializedData = JsonSerializer.Serialize(transactionData);
+
+                lock (locker)
+                {
+
+                    AddRecordToFileData(new string[] { serializedData });
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                // TODO: log
+            }
+
+            return false;
         }
 
         public List<TransactionData> Retrieve()
         {
-            //TODO:
-            throw new NotImplementedException();
+            try
+            {
+                var records = new List<TransactionData>();
+
+                var existingRawRecords = GetFileData();
+                foreach (var rawRecord in existingRawRecords)
+                {
+                    var record = JsonSerializer.Deserialize<TransactionData>(rawRecord);
+                    records.Add(record);
+                } // for
+
+                return records;
+            }
+            catch (Exception ex)
+            {
+                // TODO: log
+            }
+
+            return null;
+        }
+
+        private string[] GetFileData()
+        {
+            var records = File.ReadAllLines("asds");
+            return records;
+        }
+
+        private bool AddRecordToFileData(string[] records)
+        {
+            File.AppendAllLines("asds", records);
+            return true;
         }
     }
 }
